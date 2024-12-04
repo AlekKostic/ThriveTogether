3132f8e17451cfdd46467dbc23ba9086b925da1e
AlekKostic <kostic.aleksandar006@gmail.com>
2024-12-04 16:31:07 +0100

    Ispravka Front i Backend dela za funkcionalnost API za singup

--- Backend (1)/Backend/src/main/java/com/example/Backend/Controller/UserController.java
+++ Backend (1)/Backend/src/main/java/com/example/Backend/Controller/UserController.java
@@ -4,11 +4,9 @@ import com.example.Backend.Model.User;
 import com.example.Backend.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
