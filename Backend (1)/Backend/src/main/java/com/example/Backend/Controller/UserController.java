d06e11d379f53051df5b7adabe1d594f82727918
AlekKostic <kostic.aleksandar006@gmail.com>
2024-12-04 20:30:57 +0100

    Dodavanje Login Funkcionalnosti

--- Backend (1)/Backend/src/main/java/com/example/Backend/Controller/UserController.java
+++ Backend (1)/Backend/src/main/java/com/example/Backend/Controller/UserController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/users")
@@ -17,5 +19,16 @@ public class UserController {
     public ResponseEntity<User> createUser(@RequestBody User user){
         User savedUser = userService.saveUser(user);
         return ResponseEntity.ok(savedUser);
+    }
+
+    @GetMapping(value = "/login/{username}/{password}")
+    public ResponseEntity<Optional<User>> findUser(@PathVariable("username") String  username, @PathVariable("password") String password){
+
+        Optional<User> user = userService.findUser(username,password);
+        if(user.isPresent()){
+            return ResponseEntity.ok(user);
+        }else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
