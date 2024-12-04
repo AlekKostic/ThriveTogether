cbae32fe1b2b1fe9b902fa9d4f3689c1056a7c1e
AlekKostic <kostic.aleksandar006@gmail.com>
2024-12-04 13:13:18 +0100

    Popravak Route

---
+++ Backend (1)/Backend/src/main/java/com/example/Backend/Model/User.java
@@ -0,0 +1,66 @@
+package com.example.Backend.Model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Table(name = "User")
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String ime;
+    private String prezime;
+    private String email;
+    private String password;
+    private String username;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
