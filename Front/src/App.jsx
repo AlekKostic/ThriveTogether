cea66b1a081b9ac5ddb1a0d7df352a88ca483349
LukaBrkic23 <luka.brkic9maj@gmail.com>
2024-12-05 00:15:07 +0100

    Povezivanje ruta sa Home

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -3,13 +3,9 @@ import Signinpage from './Pages/Signinpage';
 import Emotionform from './Components/Emotionform';
 import Loginpage from './Pages/Loginpage';
 import Home from './Pages/Home';
-<<<<<<< HEAD
 import Forum from './Pages/Forum';
 import Statistika from './Pages/Statistika';
 import Kursevi from './Pages/Kursevi';
-=======
-import ForumPostForm from './Components/ForumPostForm';
->>>>>>> 5872b843d19218739baeea5bce1795860a4be1fe
 
 const App = () => {
   return (<>
@@ -17,14 +13,10 @@ const App = () => {
       <Routes>
         <Route path="/signup" element={<Signinpage />} />
         <Route path="/login" element={<Loginpage />} /> {/* Ruta za login */}
-<<<<<<< HEAD
         <Route path="/" element={<Home />} />
         <Route path="/forum" element={<Forum />} />
         <Route path="/statistika" element={<Statistika />} />
         <Route path="/kursevi" element={<Kursevi />} />
-=======
-       {/* <Route path="/" element={<Home />} />*/}
->>>>>>> 5872b843d19218739baeea5bce1795860a4be1fe
       </Routes>
     </Router>
       <ForumPostForm />
