1e2c3463ccd6ec230ec19f62018492a7a10162ac
sstojilkovic19 <marinalara2812@gmail.com>
2024-12-05 03:07:13 +0100

    dodati odgovori

--- Front/src/Components/ForumPost.jsx
+++ Front/src/Components/ForumPost.jsx
@@ -1,9 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { MdOutlineExpandMore } from "react-icons/md";
+import Reply from "./Reply";
 
 const ForumPost = ({ post }) => {
+  const [odg, setOdg] = useState([
+    {
+      id_odgovora: 2,
+      odgovor: "test2",
+      pitanja: {
+        id_pitanja: 1,
+        head: "test",
+        user: {
+          id: 6,
+          ime: "test",
+          prezime: "prezime",
+          email: "test@test.com",
+          password: "jeuju",
+          username: "test"
+        }
+      }
+    },
+    {
+      id_odgovora: 3,
+      odgovor: "test3",
+      pitanja: {
+        id_pitanja: 1,
+        head: "test",
+        user: {
+          id: 6,
+          ime: "test",
+          prezime: "prezime",
+          email: "test@test.com",
+          password: "jeuju",
+          username: "test"
+        }
+      }
+    }
+  ]);
+
+  const [showComments, setShowComments] = useState(false);
+
   const handleShowComments = () => {
-    return;
+    setShowComments(!showComments); // Toggle the visibility of comments
   };
 
   return (
@@ -13,9 +51,17 @@ const ForumPost = ({ post }) => {
       <div className="dugmecom">
         <button className="showcom" onClick={handleShowComments}>
           <MdOutlineExpandMore />
-          Prikazi komentare
+          {showComments ? "Sakrij komentare" : "Prikazi komentare"}
         </button>
       </div>
+      {/* Render comments conditionally */}
+      {showComments && (
+        <div className="comments">
+          {odg.map((o) => (
+            <Reply key={o.id_odgovora} odg={o.odgovor} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
--- Front/src/Components/Navbar.jsx
+++ Front/src/Components/Navbar.jsx
@@ -18,12 +18,15 @@ const Navbar = () => {
   const handleKurseviRedirect = () => {
     navigate('/kursevi');
   };
+  const handleHomeRedirect = () => {
+    navigate('/');
+  };
   console.log(localStorage.getItem('userID'));
   return (
     <nav className="navbar">
       <div className="navbar-left">
         {/*<ul className="sidenav">*/}
-        <p id="#NazivSajta">MemoCare</p>
+        <a id="#NazivSajta" onClick={handleHomeRedirect}>MemoCare</a>
         <a href="#forum" onClick={handleForumRedirect}>Forum</a>
         <a href="#statistika" onClick={handleStatistikaRedirect}>Statistika</a>
         <a href="#kursevi" onClick={handleKurseviRedirect}>Kursevi</a>
